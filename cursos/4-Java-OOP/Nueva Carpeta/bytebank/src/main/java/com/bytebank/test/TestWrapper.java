f63cd08daf3824da19af5ae5086229f4bc16611e
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-09-16 09:37:05 -0600

    update curso java OPP
    Update curso Java OPP

---
+++ cursos/4-Java-OOP/Nueva Carpeta/bytebank/src/main/java/com/bytebank/test/TestWrapper.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bytebank.test;
+
+/**
+ *
+ * @author miguel
+ */
+public class TestWrapper {
+
+    public static void main(String[] args){
+        Integer ref = Integer.valueOf("3");
+        ref++;
+        System.out.println(ref);
+    }
+}
